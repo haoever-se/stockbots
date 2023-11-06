e27f41d0e0516daa0ba0a2e7b8c9372ca3ad3b26
Tzuhao Chen <hao@TzuhaodeMacBook-Pro.local>
2023-11-06 16:08:08 -0500

    pylint fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 """Entry point of the server"""
-import requests
-from flask import Flask, request
 import os
 import logging
-from slack_sdk import WebClient
 import json
+from flask import Flask, request
+import requests
+from slack_sdk import WebClient
 
 from slack_sdk.errors import SlackApiError
 
@@ -23,26 +23,30 @@ def hello_world():
 
 @app.route('/slack/check', methods=['POST'])
 def slack_check():
+    """Endpoint to check stock price from a Slack command."""
     try:
         return do_slack_check(request.form)
+    # pylint: disable=W0718
     except Exception as e:
         logger.error(str(e), exc_info=True)
         return "Something went wrong!", 500
 
 
 def do_slack_check(form_data):
-    API_KEY = os.environ.get('API_KEY')
-    TOKEN = os.environ.get('TOKEN')
-    CHANNEL = os.environ.get('CHANNEL')
+    """Process the /slack/check command."""
+    api_key = os.environ.get('API_KEY')
+    token = os.environ.get('TOKEN')
+    channel_id = os.environ.get('CHANNEL')
 
     stock = form_data.get('text')
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={API_KEY}"
+    url = (f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+           f"{stock}&apikey={api_key}")
 
-    logger.debug(f"[StockPriceQueryController] request :{form_data}")
-    response = requests.get(url)
+    logger.debug("StockPriceQueryController request: %s", form_data)
+    response = requests.get(url, timeout=10)
     response.raise_for_status()
     resp = response.text
-    logger.debug(f"resp: {resp}")
+    logger.debug("Response: %s", resp)
 
     res = json.loads(resp)
     last_date = res["Meta Data"]["3. Last Refreshed"]
@@ -64,15 +68,15 @@ def do_slack_check(form_data):
     ]
 
     try:
-        response = WebClient(token=TOKEN).chat_postMessage(
-            channel=CHANNEL,
+        response = WebClient(token=token).chat_postMessage(
+            channel=channel_id,
             blocks=message_blocks
         )
         logger.debug("Message posted to Slack")
         return "Message posted to Slack"
     except SlackApiError as e:
         error_message = f"Error posting message to Slack: {e.response['error']}"
-        logger.error(error_message)
+        logger.error("Slack API error: %s", error_message)
         return error_message
 
 
