e40c96fe3a9f558bc9d9fdda1be03ebde1fd4ac1
Tzuhao Chen <hao@TzuhaodeMacBook-Pro.local>
2023-11-07 00:07:20 -0500

    feat: add symbols to db

--- app/main.py
+++ app/main.py
@@ -3,22 +3,88 @@ import os
 import logging
 import json
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy import Column, Integer, String
 import requests
 from slack_sdk import WebClient
 
 from slack_sdk.errors import SlackApiError
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'stocks.db')
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+# Setup database
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+# export FLASK_APP=app.main
+# flask db_create
+@app.cli.command('db_create')
+def db_create():
+    """Create database"""
+    db.create_all()
+    print('Database created!')
+
+
+# flask db_drop
+@app.cli.command('db_drop')
+def db_drop():
+    """Drop table"""
+    db.drop_all()
+    print('Database dropped!')
+
+
+class Symbol(db.Model):  # pylint: disable=R0903
+    """Database models"""
+    __tablename__ = 'symbol'
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String, unique=True)
+
+
+class SymbolSchema(ma.Schema):
+    """Schema definition"""
+
+    class Meta:  # pylint: disable=R0903
+        """Fields"""
+        fields = ('id', 'symbol')
+
+
+symbol_schema = SymbolSchema()
+symbols_schema = SymbolSchema(many=True)
 
 
 @app.route("/")
 def hello_world():
     """Health check function"""
     return "Hello, World!"
+
+
+@app.route('/slack/getAll', methods=['POST'])
+def slack_get_all():
+    """Get all symbol in database"""
+    symbol_list = Symbol.query.all()
+    result = symbols_schema.dump(symbol_list)
+    return result
+
+
+@app.route('/slack/add', methods=['POST'])
+def slack_add():
+    """Add symbol to database"""
+    symbol = request.form['text']
+    test = Symbol.query.filter_by(symbol=symbol).first()
+    if test:
+        return 'That symbol already exists.', 409
+
+    symbol = Symbol(symbol=symbol)
+    db.session.add(symbol)
+    db.session.commit()
+    return 'symbol added successfully!', 201
 
 
 @app.route('/slack/check', methods=['POST'])
