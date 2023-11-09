cf7fff91d2460945dac154fba9dc3a992f289a6e
Tzuhao Chen <hao@TzuhaodeMacBook-Pro.local>
2023-11-09 15:09:28 -0500

    feat: check/add in slack

--- app/main.py
+++ app/main.py
@@ -1,9 +1,37 @@
 """Entry point of the server"""
 from flask import Flask
+from db import db, setup_database
+from slack import slack_blueprint
 
 app = Flask(__name__)
+# Setup database
+setup_database(app)
+# Setup Flask Blueprints
+app.register_blueprint(slack_blueprint)
+
+
+# export FLASK_APP=app.main
+# flask db_create
+@app.cli.command('db_create')
+def db_create():
+    """Create database"""
+    db.create_all()
+    print('Database created!')
+
+
+# flask db_drop
+@app.cli.command('db_drop')
+def db_drop():
+    """Drop table"""
+    db.drop_all()
+    print('Database dropped!')
+
 
 @app.route("/")
 def hello_world():
     """Health check function"""
     return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
